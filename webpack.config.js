34d956d003a2e88cdd2ffeef91f250d898e2c642
linkerGitHub <linker-junhao@outlook.com>
2020-03-16 17:50:05 +0800

    1.调整webpack配置，修复环境参数无法传入的问题。

--- webpack.config.js
+++ webpack.config.js
@@ -9,142 +9,146 @@ const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 // const postcssPrefixer = require('postcss-prefixer')({ prefix: 'prefix-' })
 
-const isDevMode = process.env.NODE_ENV === 'development'
 
-const config = {
-  devtool: isDevMode ? 'eval-source-map' : false,
-  context: path.resolve(__dirname, './src'),
-  entry: {
-    options: './options/index.js',
-    popup: './popup/index.js',
-    background: './background/index.js',
-    contentScripts: './contentScripts/index.js',
-    injectScript: './injectScript/index.js',
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '.',
-    filename: '[name].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          extractCSS: !isDevMode
-        },
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /(node_modules|bower_components)/,
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          isDevMode ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.sass$/,
-        use: [
-          isDevMode ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader?indentedSyntax',
-        ],
-      },
-      {
-        test: /\.less$/,
-        use: [{
-          loader: isDevMode ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
-        }, {
-          loader: 'css-loader',
-        }, {
-          loader: 'less-loader' // compiles Less to CSS
-        }],
-      },
-      {
-        test: /\.styl$/,
-        use: [
-          isDevMode ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
-          'stylus-loader',
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          isDevMode ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|eot|ttf|woff|woff2|svg|svgz)$/,
-        loader: 'file-loader',
-        options: {
-          name: './[name].[ext]?[hash]',
-        },
-      },
-    ],
-  },
-  resolve: {
-    alias: {
-      vue$: 'vue/dist/vue.runtime.esm.js',
-      bulma$: 'bulma/css/bulma.css',
+function exportCfgFunction (env, argv) {
+  const isDevMode = env.NODE_ENV === 'development'
+  const config = {
+    devtool: isDevMode ? 'eval-source-map' : false,
+    context: path.resolve(__dirname, './src'),
+    entry: {
+      options: './options/index.js',
+      popup: './popup/index.js',
+      background: './background/index.js',
+      contentScripts: './contentScripts/index.js',
+      injectScript: './injectScript/index.js',
     },
-    // extensions: ['.js'],
-  },
-  plugins: [
-    new VueLoaderPlugin(),
-    new CleanWebpackPlugin({
-      cleanStaleWebpackAssets: false,
-    }),
-    new CopyWebpackPlugin([
-      { from: 'assets', to: 'assets' },
-      { from: '_locales', to: '_locales'},
-      { from: 'manifest.json', to: 'manifest.json', flatten: true },
-    ]),
-    new HtmlWebpackPlugin({
-      title: 'Options',
-      template: './index.html',
-      filename: 'options.html',
-      chunks: ['options'],
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Popup',
-      template: './index.html',
-      filename: 'popup.html',
-      chunks: ['popup'],
-    }),
-  ],
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      publicPath: '.',
+      filename: '[name].js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+          options: {
+            extractCSS: !isDevMode
+          },
+        },
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /(node_modules|bower_components)/,
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            isDevMode ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.sass$/,
+          use: [
+            isDevMode ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader?indentedSyntax',
+          ],
+        },
+        {
+          test: /\.less$/,
+          use: [{
+            loader: isDevMode ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
+          }, {
+            loader: 'css-loader',
+          }, {
+            loader: 'less-loader' // compiles Less to CSS
+          }],
+        },
+        {
+          test: /\.styl$/,
+          use: [
+            isDevMode ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            'stylus-loader',
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isDevMode ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+          ],
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|eot|ttf|woff|woff2|svg|svgz)$/,
+          loader: 'file-loader',
+          options: {
+            name: './[name].[ext]?[hash]',
+          },
+        },
+      ],
+    },
+    resolve: {
+      alias: {
+        vue$: 'vue/dist/vue.runtime.esm.js',
+        bulma$: 'bulma/css/bulma.css',
+      },
+      // extensions: ['.js'],
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new CleanWebpackPlugin({
+        cleanStaleWebpackAssets: false,
+      }),
+      new CopyWebpackPlugin([
+        { from: 'assets', to: 'assets' },
+        { from: '_locales', to: '_locales'},
+        { from: 'manifest.json', to: 'manifest.json', flatten: true },
+      ]),
+      new HtmlWebpackPlugin({
+        title: 'Options',
+        template: './index.html',
+        filename: 'options.html',
+        chunks: ['options'],
+      }),
+      new HtmlWebpackPlugin({
+        title: 'Popup',
+        template: './index.html',
+        filename: 'popup.html',
+        chunks: ['popup'],
+      }),
+    ],
+  }
+
+
+  /**
+   * Adjust rendererConfig for production settings
+   */
+  if (isDevMode) {
+    config.plugins.push(
+      new webpack.HotModuleReplacementPlugin(),
+      new ExtensionReloader({
+        contentScript: 'contentScripts',
+        background: 'background',
+        extensionPage: 'popup',
+        options: 'options',
+      })
+    )
+  } else {
+    config.plugins.push(
+      new ScriptExtHtmlWebpackPlugin({
+        async: [/runtime/],
+        defaultAttribute: 'defer',
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      })
+    )
+  }
+  return config
 }
 
-/**
- * Adjust rendererConfig for production settings
- */
-if (isDevMode) {
-  config.plugins.push(
-    new webpack.HotModuleReplacementPlugin(),
-    new ExtensionReloader({
-      contentScript: 'contentScripts',
-      background: 'background',
-      extensionPage: 'popup',
-      options: 'options',
-    })
-  )
-} else {
-  config.plugins.push(
-    new ScriptExtHtmlWebpackPlugin({
-      async: [/runtime/],
-      defaultAttribute: 'defer',
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-    })
-  )
-}
-
-module.exports = config
+module.exports = exportCfgFunction

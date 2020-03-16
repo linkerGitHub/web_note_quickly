e9fc188d4498c945b3bf6eab32f9d16e4d93681b
linkerGitHub <linker-junhao@outlook.com>
2020-03-16 15:42:13 +0800

    1.调整一些提示信息

--- src/options/App.vue
+++ src/options/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="body">
-    <p>{{ chrome.i18n.getMessage('underConstruction') }}<a target='blank' href='http://www.baidu.com'>linkerGitHub</a></p>
+    <p>{{ chrome.i18n.getMessage('underConstruction') }}</p>
+    <p>{{ chrome.i18n.getMessage('moreInfoAboutTheCrx') }}</p>
+    <a target='blank' href='https://github.com/linkerGitHub/web_note_quickly'>linkerGitHub/web_note_quickly</a>
   </div>
 </template>
 
